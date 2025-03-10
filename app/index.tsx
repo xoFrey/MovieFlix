03218f9bf95a42bfbb047cb4adbe595f3251c8fd
xofrey <izel.acxr@gmail.com>
2025-03-10 15:28:17 +0100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className='flex-1 justify-center items-center'>
+      <Text className='text-5xl text-center text-light-200 font-bold'>
+        Hallo Mama :D
+      </Text>
+    </View>
+  );
+}
